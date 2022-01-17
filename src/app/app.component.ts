979354f758b934aac4ddcab013cb89bfa5084a92
Cserhati Imre Andras <cserhati.imre.andras@gmail.com>
2022-01-17 01:39:41 +0100

    completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,22 @@ import { UserService } from './service/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'The good Angular programmer';
+  userList: User[] = this.userService.list;
+  currentUser: User = new User();
 
-  constructor() {}
+  constructor(private userService: UserService) {}
 
+  onSelectUser(user: User): void {
+    this.currentUser = user;
+  }
+  onUpdateUser(user: User): void {
+    this.userService.updateUser(user);
+  }
+  onDeleteUser(user: User): void {
+    this.userService.removeUser(user);
+  }
 }
